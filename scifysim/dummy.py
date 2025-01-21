a5ea95a4dd6cdb3023ee7a33620489a753713d22
Jeremy Scott <jscott25@arizona.edu>
2025-01-21 11:56:39 -0700

    disk implementation

--- scifysim/dummy.py
+++ scifysim/dummy.py
@@ -5,25 +5,25 @@ from pathlib import Path
 import scifysim as sf
 
 parent = Path(__file__).parent.absolute()
-
-fname = str(parent/"config/test_default.ini")
 atarget = "GJ 86 A"
-def makesim(fname, target=atarget):
-    asim = sf.utilities.prepare_all(afile=fname,
-                        thetarget=target, update_params=True,
-                        seed=1, compensate_chromatic=True,
-                        verbose=False)
-    asim.point(asim.sequence[0], asim.target,
-                        refresh_array=False, disp_override=None)
 
-    asim.context = sf.analysis.spectral_context(asim.config,
-                                                verbose=False)
-    diffuse = [asim.src.sky, asim.src.UT, asim.src.warm_optics, asim.src.combiner, asim.src.cold_optics]
-    asim.diffuse = diffuse
-    return asim
-def makesim(fname, target=atarget, compensate_chromatic=True):
+# def makesim(fname, target=atarget):
+#     asim = sf.utilities.prepare_all(afile=fname,
+#                         thetarget=target, update_params=True,
+#                         seed=1, compensate_chromatic=True,
+#                         verbose=False)
+#     asim.point(asim.sequence[0], asim.target,
+#                         refresh_array=False, disp_override=None)
+
+#     asim.context = sf.analysis.spectral_context(asim.config,
+#                                                 verbose=False)
+#     diffuse = [asim.src.sky, asim.src.UT, asim.src.warm_optics, asim.src.combiner, asim.src.cold_optics]
+#     asim.diffuse = diffuse
+#     return asim
+
+def makesim(fname, target=atarget, compensate_chromatic=True, update_params=True):
     asim = sf.utilities.prepare_all(afile=fname,
-                        thetarget=target, update_params=True,
+                        thetarget=target, update_params=update_params,
                         seed=1, compensate_chromatic=compensate_chromatic,
                         verbose=False, update_start_end=False)
     asim.point(asim.sequence[0], asim.target,
@@ -36,4 +36,5 @@ def makesim(fname, target=atarget, compensate_chromatic=True):
     return asim
 
 if __name__ == '__main__':
+    fname = str(parent/"config/test_default.ini")
     asim = makesim(fname)
